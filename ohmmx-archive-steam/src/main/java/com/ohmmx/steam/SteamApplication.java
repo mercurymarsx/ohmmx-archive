479c2e0fe0589aa9ae7f111660cc6072c33c2ef6
mmx <zoux2000@gmail.com>
2023-06-11 09:48:36 +0800

    new: 初始版本

---
+++ ohmmx-archive-steam/src/main/java/com/ohmmx/steam/SteamApplication.java
@@ -0,0 +1,11 @@
+package com.ohmmx.steam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SteamApplication.class, args);
+	}
+}
