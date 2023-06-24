f06f4cc4028d86e05e3da8b8cfb8e51a97863ed2
mmx <zoux2000@gmail.com>
2023-06-24 17:20:01 +0800

    fix: 细化类扫描参数

--- ohmmx-archive-steam/src/main/java/com/ohmmx/steam/SteamApplication.java
+++ ohmmx-archive-steam/src/main/java/com/ohmmx/steam/SteamApplication.java
@@ -2,10 +2,14 @@ package com.ohmmx.steam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.ohmmx.steam.*", "com.ohmmx.common.*" }, lazyInit = true)
+@EntityScan(basePackages = { "com.ohmmx.common.entity" })
+@EnableJpaRepositories(basePackages = { "com.ohmmx.common.mapper" })
+@ComponentScan(basePackages = { "com.ohmmx.steam", "com.ohmmx.common" }, lazyInit = true)
 public class SteamApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SteamApplication.class, args);
